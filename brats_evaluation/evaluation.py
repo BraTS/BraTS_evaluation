ef837db07f48aa4a1dc65b1123bb96453e7342eb
verena <9377970+vpchung@users.noreply.github.com>
2026-06-04 17:37:45 -0700

    support MEN-RT subject ID format in regex

--- brats_evaluation/evaluation.py
+++ brats_evaluation/evaluation.py
@@ -140,11 +140,11 @@ def main():
         # Construct full paths
         reference_filepath = os.path.join(args.ref_path, ref_filename)
 
-        # Extract the 5-digit case ID (and 3-digit timepoint, if available)
-        match = re.search(r"(\d{5}(?:-\d{3})?)", ref_filename)
+        # Extract the 4,5-digit case ID (and 3-digit timepoint, if available)
+        match = re.search(r"(\d{4,5}(?:-\d{1,3})?)", ref_filename)
         if not match:
             print(
-                "Warning: Could not extract subject ID (e.g. 12345 or 12345-001) "
+                "Warning: Could not extract subject ID (e.g. 12345-000, 12345, 1234-0) "
                 f"from {ref_filename}. Skipping."
             )
             all_evaluation_results["missings"].append(ref_filename)
